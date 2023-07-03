8fb9d852911b0cde1db6b3aebd4966e6125913b1
sangdob <51118110+sangdob@users.noreply.github.com>
2023-07-04 05:40:44 +0900

    feat : project settings

--- src/main/java/com/mutsa/mini_project/MiniProjectApplication.java
+++ src/main/java/com/mutsa/mini_project/MiniProjectApplication.java
@@ -2,8 +2,10 @@ package com.mutsa.mini_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.mutsa.mini_project.repository"})
 public class MiniProjectApplication {
 
 	public static void main(String[] args) {
